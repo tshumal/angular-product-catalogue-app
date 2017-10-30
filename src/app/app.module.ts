1b371b8e76cf0fdddc4e8dc3c532537ae9bf86f3
Lingani Tshuma <tshumal@gmail.com>
2017-10-30 04:17:39 +0200

    ProductList - Added Interface, Custom Style, Custom Pipe and Filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
